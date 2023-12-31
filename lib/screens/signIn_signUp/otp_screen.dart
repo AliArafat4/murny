7cd94e249590eaf6a96ea331b855704c77e2cd59
Zexion16 <alihma188@gmail.com>
2023-12-31 14:02:29 +0300

    solve merge issues

--- lib/screens/signIn_signUp/otp_screen.dart
+++ lib/screens/signIn_signUp/otp_screen.dart
@@ -41,152 +41,163 @@ class _OTPScreenState extends State<OTPScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Padding(
-      padding: EdgeInsets.only(
-        top: 20.sp,
-      ),
-      child: Column(children: [
-        Padding(
-          padding: EdgeInsets.only(right: 20.sp),
-          child: UpSideSigninSignup(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            visibleImage: false,
-          ),
-
-          child: Column(children: [
-            Padding(
-              padding: EdgeInsets.only(right: 20.sp),
-              child: UpSideSigninSignup(
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                visibleImage: false,
+            padding: EdgeInsets.only(
+              top: 20.sp,
+            ),
+            child: Column(children: [
+              Padding(
+                padding: EdgeInsets.only(right: 20.sp),
+                child: UpSideSigninSignup(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  visibleImage: false,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 3.h,
-            ),
-            Column(
-              children: [
-                Visibility(
-                  child: isVisible
-                      ? Text(
-                          AppLocalizations.of(context)!.otpSuccessSend,
-
-                          style: const TextStyle(fontSize: 22, fontWeight: FontWeight.w400),
-                        )
-                      : Text(
-                          AppLocalizations.of(context)!.otpSuccessSend,
-                          style: const TextStyle(fontSize: 22, fontWeight: FontWeight.w400),
-                        ),
+              Column(children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 20.sp),
+                  child: UpSideSigninSignup(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    visibleImage: false,
+                  ),
+                ),
+                SizedBox(
+                  height: 3.h,
+                ),
+                Column(
+                  children: [
+                    Visibility(
+                      child: isVisible
+                          ? Text(
+                              AppLocalizations.of(context)!.otpSuccessSend,
+                              style: const TextStyle(
+                                  fontSize: 22, fontWeight: FontWeight.w400),
+                            )
+                          : Text(
+                              AppLocalizations.of(context)!.otpSuccessSend,
+                              style: const TextStyle(
+                                  fontSize: 22, fontWeight: FontWeight.w400),
+                            ),
+                    ),
+                    SizedBox(
+                      height: 1.h,
+                    ),
+                    Text(
+                        "${AppLocalizations.of(context)!.enterOTP}\nsent to ${widget.email}",
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            color: Color(0xff8E98A8), fontSize: 16),
+                        textDirection: TextDirection.rtl),
+                  ],
+                ),
+                SizedBox(height: 4.h),
+                SizedBox(
+                  width: 77.w,
+                  height: 5.5.h,
+                  child: PinFieldAutoFill(
+                    currentCode: conOtp.text,
+                    decoration: BoxLooseDecoration(
+                        strokeWidth: 0.71,
+                        radius: const Radius.circular(7.08),
+                        strokeColorBuilder:
+                            const FixedColorBuilder(Color(0xffD0D0D0))),
+                    codeLength: 6,
+                    onCodeChanged: (value) {
+                      print(value);
+                      setState(() {
+                        conOtp.text = value!;
+                        updateButtonVisibility();
+                      });
+                    },
+                  ),
                 ),
                 SizedBox(
                   height: 1.h,
                 ),
-                Text("${AppLocalizations.of(context)!.enterOTP}\nsent to ${widget.email}",
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(color: Color(0xff8E98A8), fontSize: 16),
-                    textDirection: TextDirection.rtl),
-              ],
-            ),
-            SizedBox(height: 4.h),
-            SizedBox(
-              width: 77.w,
-              height: 5.5.h,
-              child: PinFieldAutoFill(
-                currentCode: conOtp.text,
-                decoration: BoxLooseDecoration(
-                    strokeWidth: 0.71,
-                    radius: const Radius.circular(7.08),
-                    strokeColorBuilder: const FixedColorBuilder(Color(0xffD0D0D0))),
-                codeLength: 6,
-                onCodeChanged: (value) {
-                  print(value);
-                  setState(() {
-                    conOtp.text = value!;
-                    updateButtonVisibility();
-                  });
-                },
-              ),
+                Visibility(
+                    child: isVisible
+                        ? Container()
+                        : BlocConsumer<AuthBloc, AuthState>(
+                            listener: (context, state) {
+                              if (state is AuthResendOTPErrorState) {
+                                Navigator.pop(context);
+                                showErrorSnackBar(context, state.errorMsg);
+                              }
 
-            ),
-            SizedBox(
-              height: 1.h,
-            ),
-
-            Visibility(
-                child: isVisible
-                    ? Container()
-                    : BlocConsumer<AuthBloc, AuthState>(
-                        listener: (context, state) {
-                          if (state is AuthResendOTPErrorState) {
-                            Navigator.pop(context);
-                            showErrorSnackBar(context, state.errorMsg);
-                          }
-
-                          if (state is AuthResendOTPSuccessState) {
-                            Navigator.pop(context);
-                            showSuccessSnackBar(
-                                context, "OTP has been re-sent to ${widget.email} successfully");
-                          }
-                        },
-                        builder: (context, state) {
-                          return AccountText(
-                            firstText: AppLocalizations.of(context)!.reSendOtp,
-                            secondText: AppLocalizations.of(context)!.notReceiveOtp,
-                            pushNavi: () {
-                              context.read<AuthBloc>().add(AuthResendOTPEvent(email: widget.email));
+                              if (state is AuthResendOTPSuccessState) {
+                                Navigator.pop(context);
+                                showSuccessSnackBar(context,
+                                    "OTP has been re-sent to ${widget.email} successfully");
+                              }
                             },
-                          );
-                        },
-                      )),
-            const SizedBox(
-              height: 100,
-            ),
-            Visibility(
-              visible: isVisible,
-              child: SizedBox(
-                height: 54,
-                width: 340,
-                child: BlocConsumer<AuthBloc, AuthState>(
-                  builder: (context, state) {
-                    return ElevatedButton(
-                        style: ButtonStyle(
-                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))),
-                            backgroundColor: MaterialStateProperty.all(const Color(0xff252C63))),
-                        onPressed: () {
-                          context
-                              .read<AuthBloc>()
-                              .add(AuthOTPEvent(email: widget.email, otp: conOtp.text));
-                        },
-                        child: Text(
-                          AppLocalizations.of(context)!.verify,
-                          style: const TextStyle(color: Color(0xffFFFFFF), fontSize: 16),
-                        ));
-                  },
-                  listener: (context, state) {
-                    state is LoadingState ? showLoadingDialog(context: context) : const SizedBox();
-
-                if (state is AuthOTPErrorState) {
-                  Navigator.pop(context);
-                  showErrorSnackBar(context, state.errorMsg);
-                }
-
-
-                    state is AuthOTPSuccessState
-                        ? Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const GoogleMapScreen()),
-                          )
-                        : const SizedBox();
-                  },
+                            builder: (context, state) {
+                              return AccountText(
+                                firstText:
+                                    AppLocalizations.of(context)!.reSendOtp,
+                                secondText:
+                                    AppLocalizations.of(context)!.notReceiveOtp,
+                                pushNavi: () {
+                                  context.read<AuthBloc>().add(
+                                      AuthResendOTPEvent(email: widget.email));
+                                },
+                              );
+                            },
+                          )),
+                const SizedBox(
+                  height: 100,
                 ),
-              ),
-            )
-          ]),
-        ));
+                Visibility(
+                  visible: isVisible,
+                  child: SizedBox(
+                    height: 54,
+                    width: 340,
+                    child: BlocConsumer<AuthBloc, AuthState>(
+                      builder: (context, state) {
+                        return ElevatedButton(
+                            style: ButtonStyle(
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(8))),
+                                backgroundColor: MaterialStateProperty.all(
+                                    const Color(0xff252C63))),
+                            onPressed: () {
+                              context.read<AuthBloc>().add(AuthOTPEvent(
+                                  email: widget.email, otp: conOtp.text));
+                            },
+                            child: Text(
+                              AppLocalizations.of(context)!.verify,
+                              style: const TextStyle(
+                                  color: Color(0xffFFFFFF), fontSize: 16),
+                            ));
+                      },
+                      listener: (context, state) {
+                        state is LoadingState
+                            ? showLoadingDialog(context: context)
+                            : const SizedBox();
 
+                        if (state is AuthOTPErrorState) {
+                          Navigator.pop(context);
+                          showErrorSnackBar(context, state.errorMsg);
+                        }
+
+                        state is AuthOTPSuccessState
+                            ? Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const GoogleMapScreen()),
+                              )
+                            : const SizedBox();
+                      },
+                    ),
+                  ),
+                )
+              ]),
+            ])));
   }
 }
--- lib/screens/signIn_signUp/sign_in_screen.dart
+++ lib/screens/signIn_signUp/sign_in_screen.dart
@@ -31,153 +31,139 @@ class SignInScreen extends StatelessWidget {
           FocusScope.of(context).unfocus();
         },
         child: Scaffold(
-            // backgroundColor: const Color(0xffFFFFFF),
-            body: Padding(
-
-              padding: EdgeInsets.all(20.sp),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    UpSideSigninSignup(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      visibleImage: true,
-                    ),
-                    Align(
-                        alignment: currentLanguage == "ar"
-                            ? Alignment.topRight
-                            : Alignment.topLeft,
-                        child: Text(AppLocalizations.of(context)!.signIn,
-                            style: const TextStyle(
-                              fontSize: 28,
-                              color: Color(0xff252C63),
-                            )
-                            //fontWeight: FontWeight.bold),
-                            )),
-                    SizedBox(
-                      height: 8.h,
-                    ),
-
-
-                TextFieldWidget(
-                  text: AppLocalizations.of(context)!.email,
-                  typeKeyboard: TextInputType.emailAddress,
-                  scure: false,
-                  visiblePhone: false,
-                  controller: conEmail,
-                ),
-                SizedBox(
-                  height: 2.h,
-                ),
-                TextFieldWidget(
-                  text: AppLocalizations.of(context)!.password,
-                  typeKeyboard: TextInputType.visiblePassword,
-                  scure: true,
-                  visiblePhone: false,
-                  controller: conPass,
-                ),
-
-
-                    // ),
-                    SizedBox(
-                      height: 4.h,
-                    ),
-                    SizedBox(
-                      height: 54,
-                      width: 340,
-                      child: BlocConsumer<AuthBloc, AuthState>(
-                        builder: (context, state) {
-                          return PrimaryButton(
-                            isText: true,
-                            title: AppLocalizations.of(context)!.signIn,
-                            isPadding: false,
-                            onPressed: () {
-                              context.read<AuthBloc>().add(
-                                  AuthLoginEvent(email: conEmail.text, password: conPass.text));
-                            },
-                          );
-
-                        },
-                      );
+          // backgroundColor: const Color(0xffFFFFFF),
+          body: Padding(
+            padding: EdgeInsets.all(20.sp),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  UpSideSigninSignup(
+                    onTap: () {
+                      Navigator.pop(context);
                     },
-                    listener: (context, state) {
-                      state is LoadingState
-                          ? showLoadingDialog(context: context)
-                          : const SizedBox();
+                    visibleImage: true,
+                  ),
+                  Align(
+                      alignment: currentLanguage == "ar"
+                          ? Alignment.topRight
+                          : Alignment.topLeft,
+                      child: Text(AppLocalizations.of(context)!.signIn,
+                          style: const TextStyle(
+                            fontSize: 28,
+                            color: Color(0xff252C63),
+                          )
+                          //fontWeight: FontWeight.bold),
+                          )),
+                  SizedBox(
+                    height: 8.h,
+                  ),
 
-                      if (state is AuthLoginErrorState) {
-                        Navigator.pop(context);
-                        showErrorSnackBar(context, state.errorMsg);
-                      }
+                  TextFieldWidget(
+                    text: AppLocalizations.of(context)!.email,
+                    typeKeyboard: TextInputType.emailAddress,
+                    scure: false,
+                    visiblePhone: false,
+                    controller: conEmail,
+                  ),
+                  SizedBox(
+                    height: 2.h,
+                  ),
+                  TextFieldWidget(
+                    text: AppLocalizations.of(context)!.password,
+                    typeKeyboard: TextInputType.visiblePassword,
+                    scure: true,
+                    visiblePhone: false,
+                    controller: conPass,
+                  ),
 
-
-                          if (state is AuthLoginSuccessState) {
-                            Navigator.pop(context);
-                            Navigator.push(
-
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => OTPScreen(
-                                        email: conEmail.text,
-                                      )),
-
-                            );
-                          }
-                        },
-                      ),
-                    ),
-
-                SizedBox(
-                  height: 3.h,
-                ),
-                const DividerSigninSignup(),
-
-                SizedBox(
-                  height: 3.h,
-                ),
-
-                PrimaryButton(
-                  buttonColor: Colors.transparent,
-                  onPressed: () {},
-                  text: AppLocalizations.of(context)!.signUpGmail,
-                  image: 'assets/images/gmail.svg',
-                  isText: false,
-                  isPadding: false,
-                ),
-                SizedBox(
-                  height: 1.h,
-                ),
-                PrimaryButton(
-                  buttonColor: Colors.transparent,
-                  onPressed: () {},
-                  text: AppLocalizations.of(context)!.signUpApple,
-                  image: 'assets/images/Apple.svg',
-                  isText: false,
-                  isPadding: true,
-                ),
-
-
-                    SizedBox(
-                      height: 2.h,
-                    ),
-                    AccountText(
-                      firstText: AppLocalizations.of(context)!.subscribing,
-                      secondText: AppLocalizations.of(context)!.doNotHaveAccount,
-                      pushNavi: () {
-                        navigation(
-                          context: context,
-                          type: 'push',
-                          screen: SignUpScreen(),
+                  // ),
+                  SizedBox(
+                    height: 4.h,
+                  ),
+                  SizedBox(
+                    height: 54,
+                    width: 340,
+                    child: BlocConsumer<AuthBloc, AuthState>(
+                      builder: (context, state) {
+                        return PrimaryButton(
+                          isText: true,
+                          title: AppLocalizations.of(context)!.signIn,
+                          isPadding: false,
+                          onPressed: () {
+                            context.read<AuthBloc>().add(AuthLoginEvent(
+                                email: conEmail.text, password: conPass.text));
+                          },
                         );
                       },
-                    ),
-                  ],
+                      listener: (context, state) {
+                        state is LoadingState
+                            ? showLoadingDialog(context: context)
+                            : const SizedBox();
 
-                ),
-              ],
+                        if (state is AuthLoginErrorState) {
+                          Navigator.pop(context);
+                          showErrorSnackBar(context, state.errorMsg);
+                        }
+
+                        if (state is AuthLoginSuccessState) {
+                          Navigator.pop(context);
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => OTPScreen(
+                                      email: conEmail.text,
+                                    )),
+                          );
+                        }
+                      },
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: 3.h,
+                  ),
+                  const DividerSigninSignup(),
+
+                  SizedBox(
+                    height: 3.h,
+                  ),
+
+                  PrimaryButton(
+                    buttonColor: Colors.transparent,
+                    onPressed: () {},
+                    text: AppLocalizations.of(context)!.signUpGmail,
+                    image: 'assets/images/gmail.svg',
+                    isText: false,
+                    isPadding: false,
+                  ),
+                  SizedBox(
+                    height: 1.h,
+                  ),
+                  PrimaryButton(
+                    buttonColor: Colors.transparent,
+                    onPressed: () {},
+                    text: AppLocalizations.of(context)!.signUpApple,
+                    image: 'assets/images/Apple.svg',
+                    isText: false,
+                    isPadding: true,
+                  ),
+
+                  SizedBox(height: 2.h),
+                  AccountText(
+                    firstText: AppLocalizations.of(context)!.subscribing,
+                    secondText: AppLocalizations.of(context)!.doNotHaveAccount,
+                    pushNavi: () {
+                      navigation(
+                        context: context,
+                        type: 'push',
+                        screen: SignUpScreen(),
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
-        )));
+        ));
   }
 }
